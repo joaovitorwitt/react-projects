e61cae6ebeef9f862ef76b1b995ab9db70324981
João Vitor Witt <joaovitorwitt2004@gmail.com>
2023-07-12 14:28:53 -0300

    setting up todo logic

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from "react"
-import "./styles.css"
+import { useState } from "react";
+import "./styles.css";
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
 
+  function handleSubmit(e) {
+    e.preventDefault();
 
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ];
+    });
+  }
 
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item">New Item</label>
-          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" autoComplete="off"/>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+            autoComplete="off"
+          />
         </div>
 
         <button className="btn">Add</button>
@@ -21,13 +37,12 @@ export default function App() {
       <ul className="list">
         <li>
           <label htmlFor="">
-            <input type="checkbox"/>
+            <input type="checkbox" />
             Item 1
           </label>
           <button className="btn btn-danger">Delete</button>
         </li>
       </ul>
     </>
-  )
+  );
 }
-
